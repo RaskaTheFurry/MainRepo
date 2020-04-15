09fdffe83c4bad6ec75788307004c73837d3dad9
PetrMartin <martin.petr@tul.cz>
2020-04-15 21:32:06 +0200

    added mathod for area and Shapes

---
+++ ALP2_online_1/src/interfaceVariant/Circle.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaceVariant;
+
+
+
+/**
+ *
+ * @author Uzivatel
+ */
+public class Circle implements ShapeInterface { //Circle je typove kompatibilni s ShapeInterface
+    //data
+    private double r;
+
+    //public Circle(double r) {
+    private Circle(double r) {
+        this.r = r;
+    }
+    
+    /*public Circle(double d) { //tohle nejde
+        this.r = d/2;
+    }*/
+    
+    //tovarni metoda - factory method
+    public static Circle getInstanceD(double d){
+        return new Circle(d/2);
+    }
+    
+    public static Circle getInstanceR(double r){
+        return new Circle(r);
+    }
+    
+
+    public double getR() {
+        return r;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" + "r=" + r + '}';
+        //return super.toString() + String.format(" r= %.2f", r);
+    }
+    
+    @Override
+    public double computeArea(){
+        return Math.PI*r*r;
+    }
+    
+    public static void main(String[] args) {
+        Circle c1 = Circle.getInstanceR(4);
+        System.out.println(c1);
+        System.out.println(c1.computeArea());
+        
+    }
+}
